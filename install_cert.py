a8493c62c5799150c2e584dbff49e60e397d730b
Pedro Leandro Coutin Portuondo <pcoutin@pcout.in>
2018-06-01 11:28:19 -0700

    Better printing, client global to module

--- install_cert.py
+++ install_cert.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 
 import sys
+import pprint
 from cpanelconf import USER, HOST, CPANEL_HOST, PASSWORD
 from cpanelapi import client
 
 
+CLIENT = client.Client(USER, CPANEL_HOST, password=PASSWORD, cpanel=True)
+
+
+def print_res(res):
+    pprint.pprint(res)
+
+
 def install_ssl(cert_path, key_path=None, cabundle_path=None):
-    c = client.Client(USER, CPANEL_HOST, password=PASSWORD, cpanel=True)
     ssl_args = {}
 
     def try_read(fp, k):
@@ -18,13 +25,12 @@ def install_ssl(cert_path, key_path=None, cabundle_path=None):
     try_read(key_path, 'key')
     try_read(cabundle_path, 'cabundle')
 
-    ret = c.uapi('SSL', 'install_ssl', **ssl_args)
-
-    print(str(ret).encode('utf-8'))
+    return CLIENT.uapi('SSL', 'install_ssl', **ssl_args)
 
 
 def main(argv):
-    return install_ssl(*argv)
+    res = install_ssl(*argv)
+    print_res(res)
 
 
 if __name__ == '__main__':
